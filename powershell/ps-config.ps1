e0aac3efa529adc04d1fce36d8d866235d999f13
gionnito <giopp1996@gmail.com>
2025-04-20 13:40:43 -0500

    docs: translate ps-config.ps1 comments to English

--- powershell/ps-config.ps1
+++ powershell/ps-config.ps1
@@ -1,4 +1,4 @@
-# ------------ MÓDULOS
+# ------------ MODULES
 
 # Terminal Icons
 Import-Module -Name Terminal-Icons
@@ -6,12 +6,12 @@ Import-Module -Name Terminal-Icons
 # Oh My Posh
 oh-my-posh init pwsh --config "C:\Users\gionn\GIO\config-files\powershell\gio.omp.json" | Invoke-Expression
 
-# ------------ ALIAS
+# ------------ ALIASES
 
-# Generales
+# General
 New-Alias -Name t -Value New-Item
 
-# Git y GitHub
+# Git / GitHub
 New-Alias -Name g -Value GitStatus
 New-Alias -Name ga -Value GitAdd
 New-Alias -Name gmm -Value GitCommit
@@ -20,12 +20,12 @@ New-Alias -Name gg -Value GitTree
 New-Alias -Name gs -Value GitSwitch
 New-Alias -Name gsd -Value GitSwitchDetach
 
-# ------------ FUNCIONES
+# ------------ FUNCTIONS
 
-# Generales
+# General
 # ...
 
-# Git y GitHub
+# Git / GitHub
 function GitStatus { git status }
 function GitAdd { git add @args }
 function GitCommit { git commit -m @args }
