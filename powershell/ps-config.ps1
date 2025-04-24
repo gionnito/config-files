9e647004e26126474e0855e9cd59c1b890ceba64
gionnito <giopp1996@gmail.com>
2025-04-24 17:28:22 -0500

    feat: remove some Git aliases for cleanup

--- powershell/ps-config.ps1
+++ powershell/ps-config.ps1
@@ -14,12 +14,7 @@ New-Alias -Name t -Value NewItem
 
 # Git / GitHub
 New-Alias -Name g -Value GitStatus
-New-Alias -Name ga -Value GitAdd
-New-Alias -Name gmm -Value GitCommit
-New-Alias -Name gam -Value GitAddCommit
 New-Alias -Name gg -Value GitTree
-New-Alias -Name gs -Value GitSwitch
-New-Alias -Name gsd -Value GitSwitchDetach
 
 
 # ------------ FUNCTIONS
@@ -29,9 +24,4 @@ function NewItem { New-Item @args | Out-Null }
 
 # Git / GitHub
 function GitStatus { git status }
-function GitAdd { git add @args }
-function GitCommit { git commit -m @args }
-function GitAddCommit { git commit -am @args }
 function GitTree { git log --oneline --graph --decorate --all }
-function GitSwitch { git switch @args }
-function GitSwitchDetach { git switch --detach @args }
