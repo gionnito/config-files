6652ec8413aefd87f41631edfad72c9ec436f8c0
gionnito <giopp1996@gmail.com>
2025-04-24 15:30:51 -0500

    feat: update function associated with t alias

--- powershell/ps-config.ps1
+++ powershell/ps-config.ps1
@@ -6,10 +6,11 @@ Import-Module -Name Terminal-Icons
 # Oh My Posh
 oh-my-posh init pwsh --config "C:\Users\gionn\GIO\config-files\powershell\gio.omp.json" | Invoke-Expression
 
+
 # ------------ ALIASES
 
 # General
-New-Alias -Name t -Value New-Item
+New-Alias -Name t -Value NewItem
 
 # Git / GitHub
 New-Alias -Name g -Value GitStatus
@@ -20,10 +21,11 @@ New-Alias -Name gg -Value GitTree
 New-Alias -Name gs -Value GitSwitch
 New-Alias -Name gsd -Value GitSwitchDetach
 
+
 # ------------ FUNCTIONS
 
 # General
-# ...
+function NewItem { New-Item @args | Out-Null }
 
 # Git / GitHub
 function GitStatus { git status }
